aa3d2ad4c084eafef3533834576822e61f49ac96
yaksipper114 <haha53raid@gmail.com>
2023-08-07 20:06:26 -0400

    wow

--- index.js
+++ index.js
@@ -1,18 +1,67 @@
-var http = require('http');
+const http = require('http');
 const { Configuration, OpenAIApi } = require("openai");
-require('dotenv').config()
+require('dotenv').config();
+const fs = require('fs');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
+const util = require('util');
+
+//OPENAI CONFIG.
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
-
 const openai = new OpenAIApi(configuration);
+//
 
-async function runCompletion () {
+//QUERY SEARCH
+function parseQuery(queryString) {
+  var query = {};
+  var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+  for (var i = 0; i < pairs.length; i++) {
+      var pair = pairs[i].split('=');
+      query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+  }
+  return query;
+}
+//
+
+
+async function runCompletion(prompt) {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: "How are you today?",
+      prompt: prompt
     });
-    console.log(completion.data.choices[0].text);
+    return completion.data.choices[0].text;
 }
 
-runCompletion();
+//SERVER HANDLING
+var server = http.createServer(async function(req, res){
+  
+  //req.method // GET POST HEAD
+
+  let path = url.parse(req.url, true);
+  let decoder = new StringDecoder("UTF-8");
+  let buffer = "";
+  let querys;
+  console.log(path.search);
+  if (path.search != null) {
+    querys = parseQuery(path.search);
+  }
+
+
+  if(req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.createReadStream("index.html", "UTF-8").pipe(res);
+    for(const query in querys) {
+      if(querys.hasOwnProperty(query)) {
+        if(query == "prompt") {
+          document.getElementById("p1").innerHTML = await runCompletion(querys[query]);
+        }
+      }
+    }
+  }
+
+  else if(req.method === "POST"){
+  }
+});
+server.listen(8080);
